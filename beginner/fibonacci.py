41d327f7a8758961c206255a99e96531bc0ff010
kamalam8997 <shanmughapriya1997@gmail.com>
2018-07-24 09:08:18 +0530

    Create fibonacci.py

---
+++ beginner/fibonacci.py
@@ -0,0 +1,15 @@
+Num = int(input("\nPlease Enter the Range Number: "))
+i = 0
+First_Value = 0
+Second_Value = 1
+           
+# Find & Displaying Fibonacci series
+while(i < Num):
+           if(i <= 1):
+                      Next = i
+           else:
+                      Next = First_Value + Second_Value
+                      First_Value = Second_Value
+                      Second_Value = Next
+           print(Next)
+           i = i + 1
