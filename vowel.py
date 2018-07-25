c25cce0e745e02d25cb0393066343a65d3751ba0
kamalam8997 <shanmughapriya1997@gmail.com>
2018-07-25 15:36:09 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,12 @@
+st=input(" ")
+vow = ['a','e','i','o','u','A','E','I','O','U']
+flag=0
+for x in str:
+   if(x in vow):
+     flag=1
+if(flag==1):
+    print("yes")
+else:
+    print("no")
+      
+     
